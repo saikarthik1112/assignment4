fb21f0c81482eb54aa6513850c2023ba321d1a0b
saikarthik <saikarthik.b007@gmail.com>
2020-02-20 22:30:00 +0530

    Adding functionality

---
+++ Saikarthik-CleanCode/src/main/java/Saikarthik_CleanCode/Saikarthik_CleanCode.java
@@ -0,0 +1,15 @@
+package Saikarthik_CleanCode.Saikarthik_CleanCode;
+
+
+import java.io.*;
+import java.io.IOException;
+
+public class Lekhya_CleanCode  {
+	public static void main(String[] args) throws IOException
+	{
+		Simple_and_Compound_Interest interest_obj = new Simple_and_Compound_Interest();
+		interest_obj.calculateInterest();
+		House_Construction_Cost_Estimation house_obj= new House_Construction_Cost_Estimation();
+		house_obj.costCalculation();
+	}
+}
